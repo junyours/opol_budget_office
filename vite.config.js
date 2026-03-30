8b99566e2fe02462d8916f261b9f5660525865d6
LoydTruita <xcodesonic@gmail.com>
2026-03-30 12:03:17 +0800

    clean

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,3 @@
-// import { defineConfig } from 'vite';
-// import laravel from 'laravel-vite-plugin';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//     plugins: [
-//         laravel({
-//             input: ['resources/js/app.tsx'],
-//             refresh: true,
-//         }),
-//         react(),
-//     ],
-//     resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "resources/js"),
-//     },
-//   },
-// });
 import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
 import react from '@vitejs/plugin-react'
